aa52ae040ec49e33508cd47b78f916ddf8fb71d8
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2020-10-06 14:29:21 -0400

    Update processing_and_WNT_alignment.R

--- processing_and_WNT_alignment.R
+++ processing_and_WNT_alignment.R
@@ -1,123 +1,124 @@
 library(Seurat)
 library(dplyr)
 library(Matrix)
+library(cowplot)
 
-## First steps of data processing were done with Seurat V2, dataset was update prior
-## to alignment and further processing, as shown below
-agg.data <- Read10X(data.dir = "hg19/")
+# Workflow using V3.2.2
+agg.data <- Read10X(data.dir = "./hg19/")
 agg <- CreateSeuratObject(agg.data, assay = "RNA", min.cells = 3, min.features = 200, project = "CHIR-IWP")
-agg[["percent.mt"]] <- PercentageFeatureSet(agg, pattern = "^MT-")
-agg <- subset(agg, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mt < 10)
-
-# Normalize data and find variable genes
-agg <- NormalizeData(object = agg, normalization.method = "LogNormalize", scale.factor = 10000)
-agg <- FindVariableGenes(object = agg, mean.function = ExpMean, dispersion.function = LogVMR, x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
-
-# Regression
-agg <- ScaleData(object = agg, vars.to.regress = c("nUMI", "percent.mito"))
-
-# Find significant PCs
-agg <- RunPCA(object = agg, pc.genes = agg@var.genes, do.print = TRUE, pcs.print = 1:5, genes.print = 5)
-PCElbowPlot(object = agg)
-
-# Dimension reduction
-agg <- FindClusters(object = agg, reduction.type = "pca", dims.use = 1:20, resolution = 0.6, print.output = 0, save.SNN = TRUE)
-agg <- RunTSNE(object = agg, dims.use = 1:20, do.fast = TRUE)
-
-
-
-## All steps going forward are using Seurat V3
-agg <- UpdateSeuratObject(agg)
-save(agg, file = "agg.RData")
-
+agg[["percent.mito"]] <- PercentageFeatureSet(agg, pattern = "^MT-")
+agg <- subset(agg, subset = nFeature_RNA > 200 & nFeature_RNA < 6000 & percent.mito < 10)
+agg <- NormalizeData(agg, normalization.method = "LogNormalize", scale.factor = 10000)
+agg <- FindVariableFeatures(agg, selection.method = "vst")
+all.genes <- rownames(agg)
+agg <- ScaleData(agg, features = all.genes)
+agg <- RunPCA(agg, features = VariableFeatures(object = agg))
+pdf()
+ElbowPlot(agg)
+dev.off()
+agg <- FindNeighbors(agg, dims = 1:6)
+agg <- FindClusters(agg, resolution = 0.5)
+ID <- read.table("barcode_IDs.txt", header = TRUE, sep ="\t")
+IDonly <- ID$Cell.ID
+agg <- AddMetaData(agg, metadata = IDonly, col.name = "Cell.ID")
 
 # Fig. S2A
 library(viridis)
 pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "nFeature_RNA") + theme(legend.position = "none") + scale_color_viridis(discrete=TRUE)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "nCount_RNA") + theme(legend.position = "none") + scale_color_viridis(discrete=TRUE)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "percent.mito") + theme(legend.position = "none") + scale_color_viridis(discrete=TRUE)
+VlnPlot(agg, group.by = "orig.ident", features = c("nFeature_RNA", "nCount_RNA", "percent.mito"), ncol = 3, pt.size = 0)
 dev.off()
 
 # Fig. S2B, left panel
 agg <- RunUMAP(agg, dims = 1:6)
 pdf()
-DimPlot(agg, reduction = "umap", pt.size = 1.5, group.by ="Cell.ID")
+DimPlot(agg, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f"))
+DimPlot(agg, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f")) + NoLegend()
 dev.off()
+save(agg, file = "CHIR-IWP-before-integration.RData")
 
-# Split combined dataset for integration
+####################################################################
+## Integrate CHIRSB & IWP2 samples
+# 22732 features chosen so cell cycle genes will be included for downstream regression
 agg.list <- SplitObject(agg, split.by = "Cell.ID")
 for (i in 1:length(agg.list)) {
     agg.list[[i]] <- NormalizeData(agg.list[[i]], verbose = FALSE)
     agg.list[[i]] <- FindVariableFeatures(agg.list[[i]], selection.method = "vst",
-        nfeatures = 2000, verbose = FALSE)
+        nfeatures = 22732, verbose = FALSE)
 }
 
-# Integrate CHIRSB & IWP2 samples
 reference.list <- agg.list[c("CHIRSB", "IWP2")]
-agg.anchors <- FindIntegrationAnchors(object.list = reference.list, dims = 1:26)
-agg.integrated <- IntegrateData(anchorset = agg.anchors, dims = 1:26)
-
-# Process integrated dataset
+agg.anchors <- FindIntegrationAnchors(object.list = reference.list, dims = 1:30)
+all_genes <- Reduce(intersect, lapply(reference.list, rownames))
+agg.integrated <- IntegrateData(anchorset = agg.anchors, dims = 1:30, features.to.integrate = all_genes)
 DefaultAssay(agg.integrated) <- "integrated"
 agg.integrated <- ScaleData(agg.integrated, verbose = FALSE)
 agg.integrated <- RunPCA(agg.integrated, npcs = 30, verbose = FALSE)
 pdf()
-ElbowPlot(meso)
+ElbowPlot(agg.integrated)
 dev.off()
-agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:26)
-agg.integrated <- FindNeighbors(agg.integrated, reduction = "pca", dims = 1:26)
+agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:6)
+agg.integrated <- FindNeighbors(agg.integrated, reduction = "pca", dims = 1:6)
 agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
+
 
 # Fig. S2B, right panel
 pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75)
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB")
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB") + NoLegend()
 dev.off()
 
 # Save file
-save(agg.integrated, file = "agg-integrated-26dim.RData")
+save(agg.integrated, file = "CHIR-IWP-integrated.RData")
 
 
-
+#######################################################################
 ## Cell Cycle Regression Setup
 # Assign cell cycle scores
+cc.genes <- readLines(con = "regev_lab_cell_cycle_genes.txt")
 s.genes <- cc.genes[1:43]
 g2m.genes <- cc.genes[44:97]
+agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), ndims.print = 1:10, nfeatures.print = 10)
 agg.integrated <- CellCycleScoring(agg.integrated, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
 agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
 
-# Fig. S2C, no regression
+# Fig. S2Ci, no regression
 pdf()
 DimPlot(agg.integrated, reduction = "pca")
 dev.off()
 
 
-
 ## G2M/S (Partial) Cell Cycle Regression
-agg.integrated <- CellCycleScoring(agg.integrated, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
 agg.integrated$CC.Difference <- agg.integrated$S.Score - agg.integrated$G2M.Score
 agg.integrated <- ScaleData(agg.integrated, vars.to.regress = "CC.Difference", features = rownames(agg.integrated))
 agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), nfeatures.print = 10)
 agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
 
-# Fig. S2C, G2M/S regression, PCA plot
+# Fig. S2Ci, G2M/S regression, PCA plot
 pdf()
-DimPlot(agg.integrated, reduction = "pca")
+DimPlot(agg, reduction = "pca")
 dev.off()
 
 # Finishing clustering partial regression
----agg.integrated <- RunPCA(agg.integrated)
-agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:26)
+agg.integrated <- RunPCA(agg.integrated)
+agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:6)
+agg.integrated <- FindNeighbors(agg.integrated, dims = 1:6)
+agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
 pdf()
 DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75)
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID")
 dev.off()
-save(agg.integrated, file = "agg-integrated-cclite.RData")
-
+save(agg.integrated, file = "CHIR-IWP-integrated-cclite.RData")
 
 
 ## Full Regression; reload dataset to undo partial regression
-load("agg-integrated-26dim.RData")
+load("CHIR-IWP-integrated.RData")
+cc.genes <- readLines(con = "regev_lab_cell_cycle_genes.txt")
+s.genes <- cc.genes[1:43]
+g2m.genes <- cc.genes[44:97]
+agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), ndims.print = 1:10, nfeatures.print = 10)
+agg.integrated <- CellCycleScoring(agg.integrated, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
+agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
 agg.integrated <- ScaleData(agg.integrated, vars.to.regress = c("S.Score", "G2M.Score"), features = rownames(agg.integrated))
 agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), nfeatures.print = 10)
 agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
@@ -125,15 +126,20 @@ agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
 # Fig. S2C, full regression
 pdf()
 DimPlot(agg.integrated, reduction = "pca")
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
 dev.off()
 
 # Finishing clustering full regression
----agg.integrated <- RunPCA(agg.integrated)
-agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:26)
+agg.integrated <- RunPCA(agg.integrated)
+agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:6)
+agg.integrated <- FindNeighbors(agg.integrated, dims = 1:6)
+agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
 pdf()
 DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75)
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
+DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID")
 dev.off()
-save(agg.integrated, file = "agg-integrated-cc-clusters.RData")
+save(agg.integrated, file = "CHIR-IWP-integrated-ccfull.RData")
 
 # Fig. S2D
 pdf()
