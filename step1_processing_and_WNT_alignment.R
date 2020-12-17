517eb4badad844f31a88dee8e988d8d6510ed942
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2020-12-17 12:56:17 -0500

    Update step1_processing_and_WNT_alignment.R

--- step1_processing_and_WNT_alignment.R
+++ step1_processing_and_WNT_alignment.R
@@ -32,10 +32,12 @@ dev.off()
 # Fig. S2B, left panel
 agg <- RunUMAP(agg, dims = 1:50)
 pdf()
-DimPlot(agg, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f"))
-DimPlot(agg, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f")) + NoLegend()
+DimPlot(agg, reduction = "umap", pt.size = 1, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f")) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20))
+DimPlot(agg, reduction = "umap", pt.size = 1, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f")) + NoLegend() +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20))
 dev.off()
-save(agg, file = "CHIR-IWP-before-integration-2.RData")
+save(agg, file = "CHIR-IWP-before-integration.RData")
 
 
 ####################################################################
@@ -56,7 +58,6 @@ agg.integrated <- IntegrateData(anchorset = agg.anchors, dims = 1:50, features.t
 DefaultAssay(agg.integrated) <- "integrated"
 agg.integrated <- ScaleData(agg.integrated, verbose = FALSE)
 agg.integrated <- RunPCA(agg.integrated, npcs = 50, verbose = FALSE)
-
 agg.integrated <- JackStraw(agg.integrated, num.replicate = 100, dims = 50)
 agg.integrated <- ScoreJackStraw(agg.integrated, dims = 1:50)
 pdf()
@@ -67,112 +68,83 @@ agg.integrated <- FindNeighbors(agg.integrated, reduction = "pca", dims = 1:50)
 agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
 
 
-# Fig. S2B, right panel
+# Fig. 1A (also Fig. S2B, right panel)
 pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB")
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB") + NoLegend()
+DimPlot(agg.integrated, reduction = "umap", pt.size = 1, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB") +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20))
+DimPlot(agg.integrated, reduction = "umap", pt.size = 1, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB") +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + NoLegend()
 dev.off()
 
 # Save file
-save(agg.integrated, file = "CHIR-IWP-integrated-2.RData")
+save(agg.integrated, file = "CHIR-IWP-integrated.RData")
 
 
 #######################################################################
 
-## Cell Cycle Regression Setup
-# Assign cell cycle scores
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.genes
-agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), ndims.print = 1:10, nfeatures.print = 10)
-agg.integrated <- CellCycleScoring(agg.integrated, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
-
-# Fig. S2Ci, no regression
+# Fig. S2C
 pdf()
-DimPlot(agg.integrated, reduction = "pca")
-dev.off()
-
-
-## G2M/S (Partial) Cell Cycle Regression
-agg.integrated$CC.Difference <- agg.integrated$S.Score - agg.integrated$G2M.Score
-agg.integrated <- ScaleData(agg.integrated, vars.to.regress = "CC.Difference", features = rownames(agg.integrated))
-agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), nfeatures.print = 10)
-agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
-
-# Fig. S2Ci, G2M/S regression, PCA plot
-pdf()
-DimPlot(agg, reduction = "pca")
-dev.off()
-
-# Finishing clustering partial regression
-agg.integrated <- RunPCA(agg.integrated)
-agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:6)
-agg.integrated <- FindNeighbors(agg.integrated, dims = 1:6)
-agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
-pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#e22e2f","#2a59b8"), order = "CHIRSB")
-dev.off()
-save(agg.integrated, file = "CHIR-IWP-integrated-cclite.RData")
-
-
-## Full Regression; reload dataset to undo partial regression
-load("CHIR-IWP-integrated.RData")
-cc.genes <- readLines(con = "regev_lab_cell_cycle_genes.txt")
-s.genes <- cc.genes[1:43]
-g2m.genes <- cc.genes[44:97]
-agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), ndims.print = 1:10, nfeatures.print = 10)
-agg.integrated <- CellCycleScoring(agg.integrated, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
-agg.integrated <- ScaleData(agg.integrated, vars.to.regress = c("S.Score", "G2M.Score"), features = rownames(agg.integrated))
-agg.integrated <- RunPCA(agg.integrated, features = VariableFeatures(agg.integrated), nfeatures.print = 10)
-agg.integrated <- RunPCA(agg.integrated, features = c(s.genes, g2m.genes))
-
-# Fig. S2C, full regression
-pdf()
-DimPlot(agg.integrated, reduction = "pca")
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
-dev.off()
-
-# Finishing clustering full regression
-agg.integrated <- RunPCA(agg.integrated)
-agg.integrated <- RunUMAP(agg.integrated, reduction = "pca", dims = 1:6)
-agg.integrated <- FindNeighbors(agg.integrated, dims = 1:6)
-agg.integrated <- FindClusters(agg.integrated, resolution = 0.5)
-pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Phase")
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.75, group.by = "Cell.ID", cols = c("#2a59b8","#e22e2f"), order = "CHIRSB")
-dev.off()
-save(agg.integrated, file = "CHIR-IWP-integrated-ccfull.RData")
-
-
-
-## Finish plots using either regression
-# Fig. S2D
-pdf()
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "nFeature_RNA") + NoLegend() + scale_color_viridis(discrete=TRUE)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "nCount_RNA") + NoLegend() + scale_color_viridis(discrete=TRUE)
-DimPlot(agg.integrated, reduction = "umap", pt.size = 0.5, group.by = "percent.mito") + NoLegend() + scale_color_viridis(discrete=TRUE)
+DimPlot(agg.integrated, reduction = "umap", pt.size = 1, group.by = "nFeature_RNA") + NoLegend() + scale_color_viridis(discrete=TRUE, direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+DimPlot(agg.integrated, reduction = "umap", pt.size = 1, group.by = "nCount_RNA") + NoLegend() + scale_color_viridis(discrete=TRUE, direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+DimPlot(agg.integrated, reduction = "umap", pt.size = 1, group.by = "percent.mito") + NoLegend() + scale_color_viridis(discrete=TRUE, direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
 dev.off()
 
 # Fig. 1B
+Idents(agg.integrated) <- "Cell.ID"
+WNTi <- WhichCells(agg.integrated, idents = "IWP2")
+WNTd <- WhichCells(agg.integrated, idents = "CHIRSB")
 DefaultAssay(agg.integrated) <- "RNA"
-pdf(width = 7, height = 4)
-FeaturePlot(agg.integrated, features = c("KDR"), split.by = "Cell.ID", reduction = "umap", pt.size = 0.5, min.cutoff = 0, cols = c("#000000","#f02207"))
-FeaturePlot(agg.integrated, features = c("KDR"), split.by = "Cell.ID", reduction = "umap", pt.size = 0.5, min.cutoff = 0, order = TRUE, cols = c("#000000","#f02207"))
-FeaturePlot(agg.integrated, features = c("GYPA"), split.by = "Cell.ID", reduction = "umap", pt.size = 0.5, min.cutoff = 0, order = TRUE, cols = c("#000000","#f02207"))
-FeaturePlot(agg.integrated, features = c("CDX4"), split.by = "Cell.ID", reduction = "umap", pt.size = 0.5, min.cutoff = 0, order = TRUE, cols = c("#000000","#f02207"))
+pdf()
+FeaturePlot(agg.integrated, cells = WNTi, features = c("KDR"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+FeaturePlot(agg.integrated, cells = WNTd, features = c("KDR"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+
+FeaturePlot(agg.integrated, cells = WNTi, features = c("GYPA"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+FeaturePlot(agg.integrated, cells = WNTd, features = c("GYPA"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+
+FeaturePlot(agg.integrated, cells = WNTi, features = c("CDX4"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
+FeaturePlot(agg.integrated, cells = WNTd, features = c("CDX4"), reduction = "umap", pt.size = 1.5, min.cutoff = 0, order = TRUE) + scale_color_viridis(direction = -1) +
+theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 20)) + ggtitle(element_blank())
 dev.off()
 
-# Fig. S2E
+# Supp Table A
+agg.integrated <- SetIdent(agg.integrated, cells = WNTi, value = 'WNTi')
+agg.integrated <- SetIdent(agg.integrated, cells = WNTd, value = 'WNTd')
+markers <- FindAllMarkers(agg.integrated, only.pos = TRUE, logfc = 0.176)
+write.table(markers, file="all-markers.txt", sep="\t")
+pdf()
+plot(density(agg.integrated@assays$RNA@data['CDX4',])) #mesoderm
+abline(v=0.2)
+plot(density(agg.integrated@assays$RNA@data['GYPA',])) #endoderm
+abline(v=0.1)
+plot(density(agg.integrated@assays$RNA@data['KDR',])) #endoderm
+abline(v=0.25)
+dev.off()
+WNTd.CDX4 <- WhichCells(agg.integrated, expression = KDR > 0.25 & CDX4 > 0.2, idents = "WNTd")
+WNTi.GYPA <- WhichCells(agg.integrated, expression = KDR > 0.25 & GYPA > 0.1, idents = "WNTi")
+agg.integrated <- SetIdent(agg.integrated, cells = WNTd.CDX4, value = 'WNTd.CDX4')
+agg.integrated <- SetIdent(agg.integrated, cells = WNTi.GYPA, value = 'WNTi.GYPA')
+agg.integrated[["subsets"]] <- Idents(agg.integrated)
+CDX.markers <- FindMarkers(agg.integrated, ident.1 = "WNTd.CDX4", ident.2 = "WNTi.GYPA", only.pos = TRUE, logfc = 0.176)
+GYP.markers <- FindMarkers(agg.integrated, ident.2 = "WNTd.CDX4", ident.1 = "WNTi.GYPA", only.pos = TRUE, logfc = 0.176)
+write.table(CDX.markers, file="cdx-markers.txt", sep="\t")
+write.table(GYP.markers, file="gyp-markers.txt", sep="\t")
+
+
+# Fig. S2D
 DefaultAssay(agg.integrated) <- "RNA"
 Idents(agg.integrated) <- "Cell.ID"
-wntd <- WhichCells(object = agg.integrated, expression = KDR > 0.1 & GYPA < 1, idents = "CHIRSB")
-wnti <- WhichCells(object = agg.integrated, expression = KDR > 0.1 & GYPA > 1, idents = "IWP2")
-agg.integrated <- SetIdent(object = agg.integrated, cells = wnti, value = 'WNTi')
-agg.integrated <- SetIdent(object = agg.integrated, cells = wntd, value = 'WNTd')
+WNTd2 <- WhichCells(agg.integrated, expression = KDR > 0.1 & GYPA < 1, idents = "CHIRSB")
+WNTi2 <- WhichCells(agg.integrated, expression = KDR > 0.1 & GYPA > 1, idents = "IWP2")
+agg.integrated <- SetIdent(agg.integrated, cells = WNTi2, value = 'WNTi GYPA+')
+agg.integrated <- SetIdent(agg.integrated, cells = WNTd2, value = 'WNTd GYPA-')
 p1 <- VlnPlot(agg.integrated, features = c("CYP26A1"), idents = "WNTi", pt.size = 0.5, log = TRUE, cols = c("#e12e2e")) + NoLegend() + FontSize(x.title = 10, y.title = 6) + theme(plot.title = element_text(size=10), axis.text.x = element_text(angle = 0)) + scale_y_continuous(trans="log2")
 p2 <- VlnPlot(agg.integrated, features = c("ALDH1A2"), idents = "WNTi", pt.size = 0.5, log = TRUE, cols = c("#e12e2e")) + NoLegend() + FontSize(x.title = 10, y.title = 6) + theme(plot.title = element_text(size=10), axis.text.x = element_text(angle = 0)) + scale_y_continuous(trans="log2")
 p3 <- VlnPlot(agg.integrated, features = c("CYP26A1"), idents = "WNTd", pt.size = 0.5, log = TRUE, cols = c("#2959b9")) + NoLegend() + FontSize(x.title = 10, y.title = 6) + theme(plot.title = element_text(size=10), axis.text.x = element_text(angle = 0)) + scale_y_continuous(trans="log2")
