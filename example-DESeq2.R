2c0d5b65370ac5122e34eafc4d5dc5b69ca20c54
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2021-01-25 20:38:10 -0500

    Update example-DESeq2.R

--- example-DESeq2.R
+++ example-DESeq2.R
@@ -6,11 +6,9 @@ library("limma")
 
 #Reads in a count table/matrix of genes in rows with samples in their own columns
 
-bckCountTable <- read.table("RAi-Pr1.txt", header=TRUE) #A
-bckCountTable <- read.table("RAi-SP.txt", header=TRUE) #B
-bckCountTable <- read.table("RAd-Pr1.txt", header=TRUE) #C
-bckCountTable <- read.table("RAd-SP.txt", header=TRUE) #D
-bckCountTable <- read.table("HPC-Pr1-SP.txt", header=TRUE) #E
+bckCountTable <- read.table("RAiHE-AGM34.txt", header=TRUE) #A
+bckCountTable <- read.table("RAdHE-AGM34.txt", header=TRUE) #B
+
 x=bckCountTable[,1]
 loss=bckCountTable[,-1]
 rownames(loss)=make.names(x,unique=TRUE)
@@ -21,20 +19,11 @@ bckCountTable <- loss
 #making data frame to hold the sample names and the condition types/number of different conditions
 colnames_bck <- colnames(bckCountTable)
 
-samples <- data.frame(row.names = colnames_bck,
-condition=as.factor(c(rep("RAi-HPC",3),rep("AGM-Pr1-90neg",1)))) #A
+samples <- data.frame(row.names= colnames_bck,
+condition=as.factor(c(rep("RAi",3),rep("AGM",1)))) #A
 
-samples <- data.frame(row.names = colnames_bck,
-condition=as.factor(c(rep("RAi-HPC",3),rep("AGM-Pr1-90pos",1)))) #B
-
-samples <- data.frame(row.names = colnames_bck,
-condition=as.factor(c(rep("RAd-HPC",3),rep("AGM-Pr1-90neg",1)))) #C
-
-samples <- data.frame(row.names = colnames_bck,
-condition=as.factor(c(rep("RAd-HPC",3),rep("AGM-Pr1-90pos",1)))) #D
-
-samples <- data.frame(row.names = colnames_bck,
-condition=as.factor(c(rep("RAi-HPC",3),rep("RAd-HPC",3),rep("AGM-Pr1-90neg",1),rep("AGM-SP-90pos",1)))) #E
+samples <- data.frame(row.names= colnames_bck,
+condition=as.factor(c(rep("RAd",3),rep("AGM",1)))) #B
 
 
 #Creates a Summarized Experiment (SE) object using the matrix and design objects
@@ -59,22 +48,5 @@ res_ordered <- bck_res[order(bck_res$padj),]
 summary(res_ordered)
 
 #Save the differential gene expression list to table
-write.csv(res_ordered, file="RAiHPC-Pr1.csv")
-write.csv(res_ordered, file="RAiHPC-SP.csv")
-write.csv(res_ordered, file="RAdHPC-Pr1.csv")
-write.csv(res_ordered, file="RAdHPC-SP.csv")
-
-
-
-# calculate the rlog dispersion for data set, alternative to VSD but slower
-rld <- rlog(bckCDS_1, blind=FALSE)
-
-# calculate the varaiance stabilizing transformation, for sample to sample distance heatmap/PCA
-vsd <- vst(bckCDS_1, blind=FALSE)
-
-#generate PCA plot
-pdf()
-plotPCA(vsd, intgroup=c("condition"))
-dev.off()
-pcaData <- plotPCA(vsd, intgroup=c("condition"), returnData=TRUE)
-write.csv(pcaData, file="HPC-PCA.csv")
+write.csv(res_ordered, file="RAi-AGM34.csv") #A
+write.csv(res_ordered, file="RAd-AGM34.csv") #B
