668d1a1c5706a827afcb2a2334c04dcedc724ab8
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2021-02-22 15:18:25 -0500

    Update createZeng-and-runSingleR.R

--- SingleR/createZeng-and-runSingleR.R
+++ SingleR/createZeng-and-runSingleR.R
@@ -3,6 +3,14 @@ library(dplyr)
 library(Matrix)
 library(ggplot2)
 library(viridis)
+library(matrixStats)
+library(outliers)
+library(R.utils)
+library(pheatmap)
+
+library(devtools)
+install_github("sturgeonlab/Luff-etal-2021/SingleR/versioncontrolscripts")
+library(versioncontrolscripts)
 
 # set up Zeng dataset
 #obtain matrices from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE135202
@@ -27,14 +35,11 @@ data4$sample <- "CS13X"
 data5$sample <- "CS13D"
 data6$sample <- "CS14"
 data7$sample <- "CS15"
-
 zeng <- merge(data, y = c(data2,data3,data4,data5,data6,data7), add.cell.ids = c("CS10","CS11","CS12","CS13X","CS13D","CS14","CS15"), project = "zeng")
 zeng <- NormalizeData(zeng, normalization.method = "LogNormalize", scale.factor = 10000)
 zeng <- FindVariableFeatures(zeng, selection.method = "vst", nfeatures = 2000)
 all.genes <- rownames(zeng)
 zeng <- ScaleData(zeng, features = all.genes)
-save(zeng, file="zeng.RData")
-
 
 # identify HE and arterial endothelial cells for SingleR
 pdf()
@@ -95,15 +100,7 @@ allhe <- union(he1,c(he2,he3,he4,he5,he6,he7,he9,he10,he11))
 Idents(zeng) <- "exp"
 zeng <- SetIdent(zeng, cells = aec, value = "AEC")
 zeng <- SetIdent(zeng, cells = allhe, value = "HE")
-pdf()
-  plot(density(zeng@assays$RNA@data['GFI1B',]))
-  abline(v=0.1)
-  dev.off()
-hpc <- WhichCells(zeng, expression = RUNX1 > 0.1 & CDH5 > 0.2 & PTPRC > 0.1 & ITGA2B > 0.1 & SPN > 0.05 & GFI1B > 0.1)
-hpc <- WhichCells(zeng, expression = RUNX1 > 0.1 & CDH5 > 0.2 & PTPRC > 0.1 & ITGA2B > 0.1 & SPN > 0.05 & GFI1B > 0.1)
-zeng <- SetIdent(zeng, cells = hpc, value = "HPC")
 zeng[["newlabels"]] <- Idents(zeng)
-
 zeng <- RunPCA(zeng, npcs = 50)
 zeng <- JackStraw(zeng, num.replicate = 100, dims = 50)
 zeng <- ScoreJackStraw(zeng, dims = 1:50)
@@ -115,36 +112,29 @@ zeng <- FindClusters(zeng, resolution = 1)
 zeng <- RunUMAP(zeng, reduction = "pca", dims = 1:50)
 
 #add Zeng metadata
-write.table(zeng@meta.data, file = "meta.txt", sep = "\t")
 pheno <- read.table("phenotype.txt", header = TRUE, sep ="\t")
 phenos <- pheno$Phenotype
 zeng <- AddMetaData(zeng, metadata = phenos, col.name = "pheno")
 
-#Fig. S9A,B
+# Supplementary Fig. 9A,B
 pdf()
 DimPlot(zeng, reduction = "umap", group.by = "sample", pt.size = 2.25) +
 theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(zeng, reduction = "umap", group.by = "sample", pt.size = 2.25) + NoLegend() +
+DimPlot(zeng, reduction = "umap", group.by = "sample", pt.size = 2.25) +
+theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20)) + NoLegend()
+DimPlot(zeng, reduction = "umap", group.by = "pheno", pt.size = 2.25) +
 theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(zeng, reduction = "umap", group.by = "newlabels", order = c("AEC","HE","HPC"),
-cols = c("#c7c7c7","#ff61c3","#7cae00","#00b4f0"), pt.size = 2.25) + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(zeng, reduction = "umap", group.by = "newlabels", order = c("AEC","HE","HPC"),
-cols = c("#c7c7c7","#ff61c3","#7cae00","#00b4f0"), pt.size = 2.25) + NoLegend() + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(zeng, reduction = "umap", group.by = "pheno", pt.size = 2.25) + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(zeng, reduction = "umap", group.by = "pheno", pt.size = 2.25) + NoLegend() + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
+DimPlot(zeng, reduction = "umap", group.by = "pheno", pt.size = 2.25) +
+theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20)) + NoLegend()
+DimPlot(zeng, reduction = "umap", group.by = "newlabels", order = c("AEC","HE"),
+cols = c("#c7c7c7","#7cae00","#00b4f0"), pt.size = 2.25) +
+theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
+DimPlot(zeng, reduction = "umap", group.by = "newlabels", order = c("AEC","HE"),
+cols = c("#c7c7c7","#7cae00","#00b4f0"), pt.size = 2.25) +
+theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20)) + NoLegend()
 dev.off()
+save(zeng, file="zeng.new.RData")
 
-save(zeng, file="zeng.all.RData")
-
-
-
-
-##### SingleR portion, load versioncontrolledscripts library which contains versions of SingleR scripts that will be guaranteed to work for these analyses
-library(versioncontrolscripts)
-library(matrixStats)
-library(outliers)
-library(R.utils)
-library(pheatmap)
 
 # set up reference dataset
 name = "HE_Endo"
@@ -182,6 +172,7 @@ pdf(width = 8, height = 2)
 SingleR.DrawHeatmap(obj, top.n = Inf, clusters = obj$names, order.by.clusters = TRUE)
 SingleR.DrawHeatmap(obj, top.n = Inf, clusters = obj$names2, order.by.clusters = TRUE)
 dev.off()
+#fine tune bypassed because it does not change scores with current type setup
 
 
 # export relative correlation scores and names for each cell in cs dataset
@@ -199,7 +190,6 @@ scores$type <- type$obj.names2
 
 
 #successive sorting orders cells in aesthetically-pleasing manner
-library(dplyr)
 earlyCS <- c("CS10","CS11")
 early <- filter(scores, stage %in% earlyCS)
 early.HEC <- filter(early, type == "HE")
@@ -230,8 +220,7 @@ early.HEC.cells <- rownames(early.HEC)
 early.AEC.cells <- rownames(early.AEC)
 late.HEC.cells <- rownames(late.HEC)
 late.AEC.cells <- rownames(late.AEC)
-order <- c(early.HEC.cells,early.AEC.cells,late.HEC.cells,late.AEC.cells)
-
+order <- c(early.AEC.cells,early.HEC.cells,late.AEC.cells,late.HEC.cells)
 
 # Fig. 3Bi
 pdf(width = 8, height = 2)
@@ -244,74 +233,77 @@ write.table(datascoresrel, file="datascores.txt", sep="\t")
 write.table(order, file="cellorder.txt", sep="\t")
 write.table(type, file="type.txt", sep="\t")
 write.table(stage, file="stage.txt", sep="\t")
-#import into excel and use vlookup to match barcode with scores and type label
+# import into excel and use vlookup to match ordered barcodes with scores and type label
+# see cellorder-matched.txt for final result
 
 
-# Fig. S9Di
-hpcv <- as.vector(hpc)
-cellstoplot <- c(order,hpcv)
+# Fig. 3Ci
 genes <- c("CDH5","CD34","PECAM1","TEK","ENG","ESAM","KDR","THY1","SOX7","SOX17","CXCR4","GJA4",
-"GJA5","HEY2","DLL4","NOTCH1","EFNB2","KIT","MYB","GATA2","SPI1","RUNX1","PTPRC","SPN","ITGA2B")
+"GJA5","HEY2","DLL4","NOTCH1","EFNB2","RUNX1")
 zeng <- SetIdent(zeng, cells = early.AEC.cells, value = "early.AEC.cells")
 zeng <- SetIdent(zeng, cells = early.HEC.cells, value = "early.HEC.cells")
 zeng <- SetIdent(zeng, cells = late.AEC.cells, value = "late.AEC.cells")
 zeng <- SetIdent(zeng, cells = late.HEC.cells, value = "late.HEC.cells")
-zeng <- SetIdent(zeng, cells = hpc, value = "HPC")
 zeng[["newestlabels"]] <- Idents(zeng)
-sub2 <- SubsetData(zeng, subset.name = "newestlabels", accept.value = c("early.AEC.cells","early.HEC.cells","late.AEC.cells","late.HEC.cells","HPC"))
-sub2 <- ScaleData(sub2, features = rownames(sub2))
-Idents(sub2) <- "groups"
-my_levels <- c("group1","group2","group3","group4","group5","group6","HPC")
-levels(sub2) <- my_levels
-pdf(width = 7, height = 2.5)
-DoHeatmap(sub2, draw.lines = FALSE, disp.min = 0, raster = FALSE, features = genes, cells = cellstoplot) + theme(axis.text.y = element_text(size = 6)) + scale_fill_viridis()
-dev.off()
 
-
-# Fig. S9Dii
-pdf(width = 2.5, height = 10)
-VlnPlot(sub2, features = c("CXCR4","GJA4","GJA5","HEY2","DLL4"), log = TRUE, pt.size = 0.25, ncol = 1)
-dev.off()
-
-
-# Supplementary Table 7
-Idents(sub2) <- "newlabels"
-sub2 <- SetIdent(sub2, cells = early.AEC.cells, value = "group1")
-sub2 <- SetIdent(sub2, cells = late.AEC.cells, value = "group4")
-
+#group cells based on scores
+Idents(sub) <- "newlabels"
+sub <- SetIdent(sub, cells = early.AEC.cells, value = "group1")
+sub <- SetIdent(sub, cells = late.AEC.cells, value = "group4")
 group2 <- early.HEC[1:46,]
 group2.cells <- rownames(group2)
-sub2 <- SetIdent(sub2, cells = group2.cells, value = "group2")
-
+sub <- SetIdent(sub, cells = group2.cells, value = "group2")
 group3 <- early.HEC[47:56,]
 group3.cells <- rownames(group3)
-sub2 <- SetIdent(sub2, cells = group3.cells, value = "group3")
-
+sub <- SetIdent(sub, cells = group3.cells, value = "group3")
 group5 <- late.HEC[1:41,]
 group5.cells <- rownames(group5)
-sub2 <- SetIdent(sub2, cells = group5.cells, value = "group5")
+sub <- SetIdent(sub, cells = group5.cells, value = "group5")
 group6 <- late.HEC[42:99,]
 group6.cells <- rownames(group6)
-sub2 <- SetIdent(sub2, cells = group6.cells, value = "group6")
-
-sub2[["groups"]] <- Idents(sub2)
-
-sub2@meta.data$groups <- as.character(sub2@meta.data$groups)
-sub2@meta.data$groups[sub2@meta.data$groups == "exp"] <- "HPC"
-
-Idents(sub2) <- "groups"
-markers <- FindAllMarkers(sub2, logfc = 0.176, only.pos = TRUE)
-write.table(markers, file = "group.markers.txt", sep = "\t")
-
+sub <- SetIdent(sub, cells = group6.cells, value = "group6")
+sub[["groups"]] <- Idents(sub)
+Idents(sub) <- "groups"
+my_levels <- c("group1","group2","group3","group4","group5","group6")
+levels(sub) <- my_levels
 
 # Fig. S9C
-sub2cells <- sub2$groups
-hemat <- AddMetaData(sub2, metadata = sub2cells, col.name = "groups")
-library(tidyr)
-ID <- hemat@meta.data$groups
-ID.replaced <- replace_na(ID, "others")
-hemat <- AddMetaData(sub2, metadata = ID.replaced, col.name = "groups")
+zeng <- SetIdent(zeng, cells = early.AEC.cells, value = "group1")
+zeng <- SetIdent(zeng, cells = late.AEC.cells, value = "group4")
+zeng <- SetIdent(zeng, cells = group2.cells, value = "group2")
+zeng <- SetIdent(zeng, cells = group3.cells, value = "group3")
+zeng <- SetIdent(zeng, cells = group5.cells, value = "group5")
+zeng <- SetIdent(zeng, cells = group6.cells, value = "group6")
+zeng[["groups"]] <- Idents(zeng)
 pdf()
-DimPlot(sub2, reduction = "umap", group.by = "groups", pt.size = 2.25, cols = c("#c7c7c7","#00c094","#00b6eb","#f8766d","#c49a00","#a58aff","#53b400","#fb61d7"), order = c("group5","group6","group4","group3","group2","group1","HPC","others")) + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
-DimPlot(sub2, reduction = "umap", group.by = "groups", pt.size = 2.25, cols = c("#c7c7c7","#00c094","#00b6eb","#f8766d","#c49a00","#a58aff","#53b400","#fb61d7"), order = c("group5","group6","group4","group3","group2","group1","HPC","others")) + NoLegend() + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
+DimPlot(zeng, reduction = "umap", group.by = "groups", pt.size = 2.25, cols = c("#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7",
+"#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7",
+"#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#00b6eb","#f8766d","#c49a00","#a58aff","#53b400","#fb61d7"),
+order = c("group5","group6","group4","group3","group2","group1")) + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20))
+DimPlot(zeng, reduction = "umap", group.by = "groups", pt.size = 2.25, cols = c("#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7",
+"#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7",
+"#c7c7c7","#c7c7c7","#c7c7c7","#c7c7c7","#00b6eb","#f8766d","#c49a00","#a58aff","#53b400","#fb61d7"),
+order = c("group5","group6","group4","group3","group2","group1")) + theme(axis.title = element_text(size = 18), axis.text = element_text(size = 20)) + NoLegend()
 dev.off()
+
+# Supplementary Fig. 9Di
+sub <- ScaleData(sub, features = rownames(sub))
+tiff(width = 6500, height = 2000)
+DoHeatmap(sub, draw.lines = FALSE, disp.min = 0, raster = FALSE, size = 3, features = genes, cells = order) + theme(axis.text.y = element_text(size = 6)) + scale_fill_viridis()
+dev.off()
+
+# Supplementary Fig. 9Dii
+pdf(width = 2.5, height = 10)
+VlnPlot(sub, features = c("CXCR4","GJA4","GJA5","HEY2","DLL4"), log = TRUE, pt.size = 0.25, ncol = 1)
+dev.off()
+
+# Supplementary Table 6A
+Idents(sub) <- "groups"
+markers <- FindAllMarkers(sub, logfc = 0.176)
+write.table(markers, file = "group.markers.txt", sep = "\t")
+
+# Supplementary Table 6B
+avg <- AverageExpression(sub, assay = "RNA")
+write.table(avg, file = "avg.txt", sep = "\t")
+
+save(zeng,sub, file="zeng.sub.new.RData")
