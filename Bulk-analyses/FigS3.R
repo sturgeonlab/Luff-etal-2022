6f8ca533aee9052bb94ca6b575be1147b16954f5
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2021-03-15 11:59:57 -0400

    Update FigS3.R

--- Bulk-analyses/FigS3.R
+++ Bulk-analyses/FigS3.R
@@ -2,6 +2,15 @@ library("DESeq2")
 library("ggplot2")
 library("limma")
 
+install.packages("sva_3.39.0.tar.gz", repos = NULL, type="source")
+library("sva")
+
+library("DESeq2")
+library("ggplot2")
+library("pheatmap")
+library("limma")
+
+
 bckCountTable <- read.table("FigS3D.txt", header = TRUE)
 x = bckCountTable[,1]
 loss = bckCountTable[,-1]
@@ -11,20 +20,21 @@ bckCountTable <- loss
 colnames_bck1 <- colnames(bckCountTable)
 samples <- data.frame(row.names = colnames_bck1, condition = as.factor(c(rep("CXCR4.neg",3),rep("CXCR4.pos",3))), 
                       batch = as.factor(c(rep("1",1),rep("2",1),rep("3",1),rep("1",1),rep("2",1),rep("3",1))))
-bckCDS <- DESeqDataSetFromMatrix(countData = bckCountTable, colData = samples, design = ~ batch + condition)
+
+bckCountTable_corr <- ComBat_seq(as.matrix(bckCountTable),samples$batch)
+bckCDS <- DESeqDataSetFromMatrix(countData = bckCountTable_corr, colData = samples, design = ~ batch + condition)
 DESeq.ds <- DESeq(bckCDS)
+DESeq.rlog <- rlogTransformation(DESeq.ds, blind = TRUE)
+pdf()
+plotPCA(DESeq.rlog,ntop=300)
+dev.off()
+pcaData <- plotPCA(DESeq.rlog,ntop=300, returnData = TRUE)
+write.csv(pcaData,file = "pcaData.csv")
+
+# Supplementary Table 3B
 bck_res <- results(DESeq.ds)
 res_ordered <- bck_res[order(bck_res$padj),]
 write.csv(res_ordered, file="Table3B.csv")
-
-vsd <- vst(DESeq.ds, blind = FALSE)
-DESeq.ds_assay_corr <- limma::removeBatchEffect(assay(vsd),vsd$batch)
-assay(vsd) <- DESeq.ds_assay_corr
-pdf()
-plotPCA(vsd, intgroup = c("condition"))
-dev.off()
-pcaData <- plotPCA(vsd, intgroup = c("condition"), returnData = TRUE)
-write.csv(pcaData,file = "pcaData.csv")
 
 
 > sessionInfo()
