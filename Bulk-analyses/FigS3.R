d0dd051db6e067c416624cd895a98f1022129486
sturgeonlab <72148818+sturgeonlab@users.noreply.github.com>
2021-03-15 12:46:56 -0400

    Update FigS3.R

--- Bulk-analyses/FigS3.R
+++ Bulk-analyses/FigS3.R
@@ -54,22 +54,24 @@ attached base packages:
 [8] methods   base     
 
 other attached packages:
- [1] limma_3.40.6                ggplot2_3.3.2              
- [3] DESeq2_1.24.0               SummarizedExperiment_1.14.1
- [5] DelayedArray_0.10.0         BiocParallel_1.18.1        
- [7] matrixStats_0.55.0          Biobase_2.44.0             
- [9] GenomicRanges_1.36.1        GenomeInfoDb_1.20.0        
-[11] IRanges_2.18.3              S4Vectors_0.22.1           
-[13] BiocGenerics_0.30.0        
+ [1] sva_3.39.0                  genefilter_1.66.0          
+ [3] mgcv_1.8-31                 nlme_3.1-144               
+ [5] limma_3.40.6                pheatmap_1.0.12            
+ [7] ggplot2_3.3.2               DESeq2_1.24.0              
+ [9] SummarizedExperiment_1.14.1 DelayedArray_0.10.0        
+[11] BiocParallel_1.18.1         matrixStats_0.55.0         
+[13] Biobase_2.44.0              GenomicRanges_1.36.1       
+[15] GenomeInfoDb_1.20.0         IRanges_2.18.3             
+[17] S4Vectors_0.22.1            BiocGenerics_0.30.0        
 
 loaded via a namespace (and not attached):
- [1] bit64_0.9-7            splines_3.6.2          Formula_1.2-3         
- [4] latticeExtra_0.6-29    blob_1.2.1             GenomeInfoDbData_1.2.1
- [7] pillar_1.4.3           RSQLite_2.2.0          backports_1.1.5       
-[10] lattice_0.20-40        glue_1.4.2             digest_0.6.24         
-[13] RColorBrewer_1.1-2     XVector_0.24.0         checkmate_2.0.0       
-[16] colorspace_1.4-1       htmltools_0.4.0        Matrix_1.2-18         
-[19] XML_3.99-0.3           pkgconfig_2.0.3        genefilter_1.66.0     
+ [1] edgeR_3.28.1           bit64_0.9-7            splines_3.6.2         
+ [4] Formula_1.2-3          latticeExtra_0.6-29    blob_1.2.1            
+ [7] GenomeInfoDbData_1.2.1 pillar_1.4.3           RSQLite_2.2.0         
+[10] backports_1.1.5        lattice_0.20-40        glue_1.4.2            
+[13] digest_0.6.24          RColorBrewer_1.1-2     XVector_0.24.0        
+[16] checkmate_2.0.0        colorspace_1.4-1       htmltools_0.4.0       
+[19] Matrix_1.2-18          XML_3.99-0.3           pkgconfig_2.0.3       
 [22] zlibbioc_1.30.0        purrr_0.3.3            xtable_1.8-4          
 [25] scales_1.1.0           jpeg_0.1-8.1           htmlTable_1.13.3      
 [28] tibble_3.0.4           annotate_1.62.0        farver_2.0.3          
@@ -87,4 +89,4 @@ loaded via a namespace (and not attached):
 [64] Hmisc_4.3-1            stringi_1.4.6          Rcpp_1.0.3            
 [67] geneplotter_1.62.0     vctrs_0.3.4            rpart_4.1-15          
 [70] acepack_1.4.1          png_0.1-7              tidyselect_1.1.0      
-[73] xfun_0.12  
+[73] xfun_0.12
